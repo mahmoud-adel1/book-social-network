8a8d70ce3db9834b9f7518ec2e0c98c88630a174
Mahmoud <mahmoudadel537@gmail.com>
2025-08-17 16:13:47 +0300

    initialize docker-compose

---
+++ src/main/java/com/example/book_social_network/BookSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialNetworkApplication.class, args);
+	}
+
+}
