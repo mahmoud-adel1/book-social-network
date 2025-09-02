be68bfbbd6c77d3f456c6439ddef1657861ff985
Mahmoud <mahmoudadel537@gmail.com>
2025-09-02 03:42:20 +0300

    Create token entity

---
+++ src/main/java/com/example/book_social_network/user/Token.java
@@ -0,0 +1,28 @@
+package com.example.book_social_network.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tokens")
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
