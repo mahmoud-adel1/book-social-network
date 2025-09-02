015bcb30bf78a385b188fe84aff0d5a961814501
Mahmoud <mahmoudadel537@gmail.com>
2025-09-02 03:07:05 +0300

    create role entity

---
+++ src/main/java/com/example/book_social_network/role/Role.java
@@ -0,0 +1,23 @@
+package com.example.book_social_network.role;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String name;
+}
