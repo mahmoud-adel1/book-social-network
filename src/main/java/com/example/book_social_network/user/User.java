d73fe3d0d82fe9028ac31c0f8d70e09584c7aebb
Mahmoud <mahmoudadel537@gmail.com>
2025-09-02 03:14:51 +0300

    Update getAuthorities method

--- src/main/java/com/example/book_social_network/user/User.java
+++ src/main/java/com/example/book_social_network/user/User.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.security.auth.Subject;
@@ -56,7 +57,10 @@ public class User implements UserDetails, Principal {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return roles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
     }
 
     @Override
